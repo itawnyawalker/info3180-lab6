0c7f66397492531c4c0c6542f8ca92e2d7a1add0
Itawnya Walker <itawnya.walker@gmail.com>
2022-03-30 10:47:40 -0500

    Installed dependencies, created first component

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,12 @@
 <script>
+import NewsList from '@/components/NewsList.vue'
 export default {
     data() {
         return {
-            message: "Hello World! Welcome to VueJS"
+            message: "Booo! Welcome to VueJS"
         };
     },
+    components: { NewsList }
 }
 </script>
 
@@ -12,7 +14,8 @@ export default {
     <div class="container">
       <div class="text-center">
         <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-        <h1>{{ message }}</h1>
+        <h1>{{ message }} </h1>
+        <NewsList />
       </div>
     </div>
 </template>
